2509a65194b58b75f6fff7ff32fc4507672bc7fe
evan <evaniainbrooks@gmail.com>
2014-09-01 02:21:16 -0700

    Add user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,18 @@ class User
   field :uid, type: String
   field :name, type: String
   field :email, type: String
+  field :image, type: String
   field :ip_address, type: String
   field :coordinates, type: Array
+
+  field :belt_rank, type: String
+  field :stripe_rank, type: Integer
 
   geocoded_by :ip_address
   after_validation :geocode
 
   belongs_to :team
+  belongs_to :instructor, class_name: 'User'
 
   def self.create_with_omniauth(auth, ip_address)
     create! do |user|
