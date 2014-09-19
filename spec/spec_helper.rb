71a938fe70f55e90bb664eb31a2fe1575acf0aa5
evan <evaniainbrooks@gmail.com>
2014-09-18 18:00:38 -0700

    Config test lookup for geocoder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,21 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 Mongoid.load!('./config/mongoid.yml')
 
+Geocoder.configure(:lookup => :test)
+Geocoder::Lookup::Test.set_default_stub(
+  [
+    {
+      'latitude'     => 40.7143528,
+      'longitude'    => -74.0059731,
+      'address'      => 'New York, NY, USA',
+      'state'        => 'New York',
+      'state_code'   => 'NY',
+      'country'      => 'United States',
+      'country_code' => 'US'
+    }
+  ]
+)
+
 RSpec.configure do |config|
   # ## Mock Framework
   #
