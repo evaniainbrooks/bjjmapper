16cebdd6b323f6b3a3b0c701fde1d6a6063dd39a
evan <evaniainbrooks@gmail.com>
2014-09-24 16:16:26 -0700

    Clear filters button

--- app/assets/javascripts/backbone/views/team-list-view.js
+++ app/assets/javascripts/backbone/views/team-list-view.js
@@ -4,12 +4,13 @@
   RollFindr.Views.TeamListView = Backbone.View.extend({
     template: null,
     events: {
-      'click [type="checkbox"]': 'changeFilter'
+      'click [type="checkbox"]': 'changeFilter',
+      'click a[data-clear-filters]': 'clearFilters'
     },
     initialize: function() {
       var self = this;
 
-      _.bindAll(this, 'changeFilter');
+      _.bindAll(this, 'changeFilter', 'clearFilters');
 
       var templateContent = $('.team-list-template');
       if (templateContent.length > 0) {
@@ -35,6 +36,17 @@
         return f.get('filter-active');
       });
       return filteredCollection;
+    },
+    clearFilters: function() {
+      this.$('input:checked').each(function(i, o) {
+        $(o).removeAttr('checked');
+      });
+
+      this.collection.each(function(f) {
+        f.set('filter-active', null, {silent: true});
+      });
+
+      this.collection.trigger('sync');
     }
   });
 
