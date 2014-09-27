4fbad56c3223081305d3aca042360f5ea8495852
evan <evaniainbrooks@gmail.com>
2014-09-26 23:52:55 -0700

    Dont use its

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,9 @@ describe User do
     let(:example_ip) { '192.168.1.1' }
     let(:example_auth) { { 'provider' => 'google', 'uid' => '12345', 'info' => { 'name' => 'testname', 'email' => 'testemail' } } }
     subject { described_class.create_with_omniauth(example_auth, example_ip) }
-    its(:name) { should eq example_auth['info']['name'] }
-    its(:email) { should eq example_auth['info']['email'] }
-    its(:uid) { should eq example_auth['uid'] }
-    its(:ip_address) { should eq example_ip }
+    it { subject.name.should eq example_auth['info']['name'] }
+    it { subject.email.should eq example_auth['info']['email'] }
+    it { subject.uid.should eq example_auth['uid'] }
+    it { subject.ip_address.should eq example_ip }
   end
 end
