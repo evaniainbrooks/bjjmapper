f2e84d47f012a2d60b32dc3418284a3ee94cdf10
evan <evaniainbrooks@gmail.com>
2014-09-28 14:48:24 -0700

    Change cookie store name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_research_machine_session'
+Rails.application.config.session_store :cookie_store, key: '_rollfindr_session'
