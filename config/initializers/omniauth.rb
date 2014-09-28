1be6618fff2c27bb0c7c53a718acd71d63dd2d08
evan <evaniainbrooks@gmail.com>
2014-09-28 14:48:43 -0700

    Google+ signin integration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  require 'openid/store/filesystem' 
-  provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+  #require 'openid/store/filesystem' 
+  #provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] 
 end
