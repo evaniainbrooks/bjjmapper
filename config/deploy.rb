49cf0d88e414196423f0437649919a5dc81c149b
evan <evaniainbrooks@gmail.com>
2014-09-28 16:52:23 -0700

    Try setting umask on puma sock

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :keep_releases, 3
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock?umask=0137"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
