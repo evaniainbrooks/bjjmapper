6be1ff5af526f60792a6a1d7f28132e9e7fa4752
evan <evaniainbrooks@gmail.com>
2014-09-28 22:07:56 -0700

    ensure root for setup_server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,8 +15,7 @@ role :db,  %w{deploy@staging.bjjmapper.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'staging.bjjmapper.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
+server 'staging.bjjmapper.com', user: 'deploy', roles: %w{web app db}
 
 # Custom SSH Options
 # ==================
--- script/setup_server.sh
+++ script/setup_server.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo "* Updating system"
 apt-get update
 apt-get -y upgrade
 echo "* Installing packages"
-apt-get -y install build-essential libmagickcore-dev imagemagick libmagickwand-dev libxml2-dev libxslt1-dev git-core nginx redis-server curl nodejs htop acl nginx
+apt-get -y install build-essential libmagickcore-dev imagemagick libmagickwand-dev libxml2-dev libxslt1-dev git-core nginx curl nodejs htop acl nginx
 
 id -u deploy &> /dev/null
 
@@ -10,6 +16,8 @@ if [ $? -ne 0 ]
 then
   echo "* Creating user deploy"
   useradd -m -g staff -s /bin/bash deploy
+  echo "* Creating user rollfindr"
+  useradd -m -g staff -s /bin/bash rollfindr
   echo "* Adding user deploy to sudoers"
   chmod +w /etc/sudoers
   echo "deploy ALL=(ALL) ALL" >> /etc/sudoers
@@ -58,7 +66,7 @@ then
 fi
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQTjjPrdPMib0C8yejRT+S2fdAs/A2NSLyfBXbZNOFQWfUmjTi0hkAwcE/ahYJrmsxKJUgpMRJJcklCgxwZEcmMG7+W5QhASx00ayDRDYtJMCo/ZiePCntVYzZcefTr0ZOx3fYjXJ8IntcbIf3nwumG5Q/yE9nbwXwAo4DSxW5fveWwlG99c8LSKuKGKzl2fEsU/93LADrgj5yqnFIa5YdkDZCr80P5zJixMc1jzZ0+iYBVk9yc5Zph1WQG4Cq9wIQOT8TC+BSYWHguePfMPl2a0uqYu/r4guqu7q5/74dvFRdGc1lOIA28rbZdWIPp037tw0XDUYcP8/RisufLdUB evaniainbrooks@gmail.com" > /home/deploy/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDu9E0ooFAh/6PJNQTGzr5LNGWNmZHpahKsS3WvndUNbGlvqNKkXDOreeeyYcR96zLY3RxkUBuJ5xsKF0C3HAScDxfe83qtzuLYq937GsY60xY/c/KhtLn94mvpnFe7vSCkx/FsZzo50T2nIXrhpkRR/3TL+ntiYjWitSJwPALd+lvG/aN229zE1lZb9f55Muf0o2l7fc32Aq79yKnsqfSwOpLRjJbUoTcbWeXxz0fYhwcBinibAFSIlYEHqZ8/DR4iDileEAhv3rlqKLnJkQnWowZ3wJb+3DwbiRzY4bTqlavYcJv7fVDmm7vO3zniH50F9vjfFe6ADbjC5mUlXFtJ Codeship/rollfindr/rollfindr" > /home/deploy/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDu9E0ooFAh/6PJNQTGzr5LNGWNmZHpahKsS3WvndUNbGlvqNKkXDOreeeyYcR96zLY3RxkUBuJ5xsKF0C3HAScDxfe83qtzuLYq937GsY60xY/c/KhtLn94mvpnFe7vSCkx/FsZzo50T2nIXrhpkRR/3TL+ntiYjWitSJwPALd+lvG/aN229zE1lZb9f55Muf0o2l7fc32Aq79yKnsqfSwOpLRjJbUoTcbWeXxz0fYhwcBinibAFSIlYEHqZ8/DR4iDileEAhv3rlqKLnJkQnWowZ3wJb+3DwbiRzY4bTqlavYcJv7fVDmm7vO3zniH50F9vjfFe6ADbjC5mUlXFtJ Codeship/rollfindr/rollfindr" >> /home/deploy/.ssh/authorized_keys
 
 chmod 600 /home/deploy/.ssh/authorized_keys
 chown deploy /home/deploy/.ssh/authorized_keys
