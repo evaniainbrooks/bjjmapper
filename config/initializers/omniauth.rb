96bfa2cbb9bdc75518981c5c48e4cd37179255a3
evan <evaniainbrooks@gmail.com>
2014-10-06 18:43:12 -0700

    Add twitter,facebook to omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #require 'openid/store/filesystem' 
   #provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] 
+  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'] 
+  provider :twitter, ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET'] 
   provider :identity, :fields => [:email]
 end
