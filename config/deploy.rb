fb374a4fa248a3d0402598cc8c72d9816eb3795d
evan <evaniainbrooks@gmail.com>
2014-10-12 06:20:12 -0700

    Remove revision check from deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,11 +66,12 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse bitbucket/master`
-        puts "WARNING: HEAD is not the same as bitbucket/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
+      puts `git rev-parse HEAD`
+      #unless `git rev-parse HEAD` == `git rev-parse bitbucket/master`
+      #  puts "WARNING: HEAD is not the same as bitbucket/master"
+      #  puts "Run `git push` to sync changes."
+      #  exit
+      #end
     end
   end
 
