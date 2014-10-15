07c758fa02b71fb0f1063cd75bcdfab6c72ef200
evan <evaniainbrooks@gmail.com>
2014-10-15 01:18:06 -0700

    Omniauth identity failure action

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #require 'openid/store/filesystem' 
+  #require 'openid/store/filesystem'
   #provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] 
-  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'] 
-  provider :twitter, ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET'] 
-  provider :identity, :fields => [:email]
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
+  provider :twitter, ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET']
+  provider :identity, :fields => [:email, :name], :on_failed_registration => SessionsController.action(:new)
 end
