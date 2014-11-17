5b78a2c13f4c6112758eb5757050605b2db4632f
evan <evaniainbrooks@gmail.com>
2014-11-16 19:28:51 -0800

    Add alias method to tracker

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -8,6 +8,11 @@ module RollFindr
       tracker.track(@user_id, event, params)
     end
 
+    def alias(new_id, old_id)
+      tracker.alias(new_id, old_id)
+      @user_id = new_id
+    end
+
     private
 
     def tracker
--- spec/lib/tracker_spec.rb
+++ spec/lib/tracker_spec.rb
@@ -7,9 +7,20 @@ describe RollFindr::Tracker do
     let(:event) { 'SomeEvent' }
     let(:params) { { 'key' => 'value' } }
     subject { RollFindr::Tracker.new(uid) }
-    it 'calls track on the underlying implementation' do
-      Mixpanel::Tracker.any_instance.should_receive(:track).with(uid, event, params)
-      subject.track(event, params)
+    describe '.track' do
+      it 'calls track on the underlying implementation' do
+        Mixpanel::Tracker.any_instance.should_receive(:track).with(uid, event, params)
+        subject.track(event, params)
+      end
+    end
+    describe '.alias' do
+      it 'calls alias and sets the new user id' do
+        Mixpanel::Tracker.any_instance.should_receive(:alias).with('newuid', uid)
+        Mixpanel::Tracker.any_instance.should_receive(:track).with('newuid', event, params)
+        
+        subject.alias('newuid', uid)
+        subject.track(event, params)
+      end
     end
   end
 end
