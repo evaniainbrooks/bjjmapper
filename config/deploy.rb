cabb727a4181df94012559b988298a902c5401a1
evan <evaniainbrooks@gmail.com>
2014-11-18 14:30:38 -0800

    Changing deployment location

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@bitbucket.org:/rollfindr/rollfindr.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/rollfindr/rollfindr'
+set :deploy_to, '/var/www/rollfindr'
 
 # Default value for :scm is :git
 set :scm, :git
@@ -37,7 +37,7 @@ set :keep_releases, 3
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock?umask=0137"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
@@ -59,7 +59,14 @@ namespace :puma do
     end
   end
 
+  task :fix_socket_permissions do
+    on roles(:app) do
+      excute "chmod 777 #{shared_path}/tmp/sockets/puma.sock"
+    end
+  end
+
   before :start, :make_dirs
+  after :start, :fix_socket_permissions
 end
 
 namespace :deploy do
