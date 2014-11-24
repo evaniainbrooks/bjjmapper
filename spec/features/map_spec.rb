18500280ac59b6d7d3690e04a4424e6f09f8ae63
evan <evaniainbrooks@gmail.com>
2014-11-24 01:16:30 -0800

    Fix map spec for user agent

--- spec/features/map_spec.rb
+++ spec/features/map_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 feature "Locations map" do
+  background do
+    Capybara.current_session.driver.header('Accept-Language', 'en')
+    Capybara.current_session.driver.header('User-Agent', 'TestUserAgent')
+  end
+
   scenario "user visits the main page" do
     visit root_path
     expect(page).to have_selector('.map-canvas')
