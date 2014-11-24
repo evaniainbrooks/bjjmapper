a7a434ed1551146d5348e666933cfffffdc3a69e
evan <evaniainbrooks@gmail.com>
2014-11-24 13:20:51 -0800

    Bugfix if location does not have a team

--- script/import_locations.rb
+++ script/import_locations.rb
@@ -18,7 +18,7 @@ f = File.open(options[:filename], "r")
 csv = CSV.parse(f, :headers => false)
 puts "Running as #{Rails.env}"
 csv.each do |row|
-  team_name = row[9].strip
+  team_name = row[9].try(:strip)
   team = Team.where(:name => team_name).first_or_create
   p "Creating #{row[0]}, team is #{team.id} '#{team.name}'"
   Location.create!({:title => row[0], :street => row[1], :city => row[2], :state => row[3], :postal_code => row[4], :country => row[5], :website => row[6], :phone => row[7], :instructors => [User.where(:name => row[8].try(:strip)).first_or_create], :team => team})
