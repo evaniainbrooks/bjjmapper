a869e8dda7a36bfe19dc1046e89cefe05e189a70
evan <evaniainbrooks@gmail.com>
2014-11-24 04:37:19 -0800

    Toastr options

--- app/assets/javascripts/toastr_bootstrap.js
+++ app/assets/javascripts/toastr_bootstrap.js
@@ -1,6 +1,9 @@
 +function() {
   "use strict";
   toastr.options = {
-    positionClass: 'toast-bottom-left'
+    positionClass: 'toast-bottom-left',
+    toastClass: 'toast',
+    closeButton: true,
+    timeOut: "10000"
   };
 }();
