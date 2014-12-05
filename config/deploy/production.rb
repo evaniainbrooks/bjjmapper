f6f887a5746d96636afea49bc4c2478b671892e7
evan <evaniainbrooks@gmail.com>
2014-12-04 18:18:14 -0800

    Deploy to rollfindr.com since bjjmapper.com is now behing CloudFlare

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@bjjmapper.com}
-role :web, %w{deploy@bjjmapper.com}
-role :db,  %w{deploy@bjjmapper.com}
+role :app, %w{deploy@rollfindr.com}
+role :web, %w{deploy@rollfindr.com}
+role :db,  %w{deploy@rollfindr.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@bjjmapper.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'bjjmapper.com', user: 'deploy', roles: %w{web app db}
+server 'rollfindr.com', user: 'deploy', roles: %w{web app db}
 
 # Custom SSH Options
 # ==================
@@ -32,7 +32,7 @@ server 'bjjmapper.com', user: 'deploy', roles: %w{web app db}
 #
 # And/or per server (overrides global)
 # ------------------------------------
-# server 'bjjmapper.com',
+# server 'rollfindr.com',
 #   user: 'user_name',
 #   roles: %w{web app},
 #   ssh_options: {
