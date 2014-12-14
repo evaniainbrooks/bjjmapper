26f3ef35e80fcd455ac35d5941957e2082355f7c
evan <evaniainbrooks@gmail.com>
2014-12-14 00:13:55 -0800

    instructor helper factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -7,5 +7,13 @@ FactoryGirl.define do
     email 'test@rollfindr.com'
     image 'testimg.jpg'
     provider :twitter
+
+    factory :instructor_with_students do
+      after(:create) do |instance|
+        3.times do
+          instance.lineal_children << create(:user)
+        end
+      end
+    end
   end
 end
