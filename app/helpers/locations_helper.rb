ec80f687d15c936baf1b1418990548c685b4fa40
Evan <evan@bjjmapper.com>
2015-02-05 02:01:43 +0200

    Fix warning msg

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,6 +1,6 @@
 module LocationsHelper
   def country_name_for(country)
     return '' unless country.present?
-    country.length == 2 ? (RollFindr::DirectoryCountries.index(country) || country) : country
+    country.length == 2 ? (RollFindr::DirectoryCountries.key(country) || country) : country
   end
 end
