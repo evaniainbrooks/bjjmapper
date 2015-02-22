44ee009dd8aa0484bfb9421c8755ea9eafead868
Evan <evan@bjjmapper.com>
2015-02-22 16:33:25 +0200

    Clear avatar button logic

--- app/assets/javascripts/avatar_upload.js
+++ app/assets/javascripts/avatar_upload.js
@@ -1,8 +1,22 @@
 +function() {
   $(document).ready(function() {
+    $('body').delegate('[data-clear-avatar]', 'click', function(e) {
+      var team = $(e.currentTarget).data('id');
+      $.ajax({
+        url: Routes.remove_image_team_path(team), 
+        type: 'POST',
+        dataType: 'json',
+        success: function() {
+          toastr.success('Image processing may take a few minutes. Please check back soon.', 'Image successfully removed');
+        },
+        error: function() {
+          toastr.error('Please try again later. If you believe this to be a bug, please email us at info@bjjmapper.com', 'Image removal failed');
+        }
+      })
+      
+    });
+    
     $('body').delegate('[data-upload-avatar]', 'change', function(e) {
-      console.log('starting ajax file upload');
-
       var files = e.currentTarget.files;
       var team = $(e.currentTarget).data('id');
 
