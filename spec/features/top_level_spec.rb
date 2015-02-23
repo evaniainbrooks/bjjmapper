677eac8fc41f390b62cdfed6a5886735a4ed6f7a
Evan <evan@bjjmapper.com>
2015-02-23 14:16:37 +0200

    Move map feature spec to top level spec, add more specs for top level pages

---
+++ spec/features/top_level_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature "Top Level Pages" do
+  background do
+    Capybara.current_session.driver.header('Accept-Language', 'en')
+    Capybara.current_session.driver.header('User-Agent', 'TestUserAgent')
+  end
+
+  scenario "user visits the main page" do
+    visit root_path
+    expect(page).to have_selector('.homepage')
+  end
+  scenario "user visits the map" do
+    visit map_path
+    expect(page).to have_selector('.map-canvas')
+  end
+  scenario "user visits the directory" do
+    visit directory_index_path
+    expect(page).to have_selector('.directory')
+  end
+  scenario "user visits the meta page" do
+    visit meta_path
+    expect(page).to have_selector('.meta')
+  end
+end
