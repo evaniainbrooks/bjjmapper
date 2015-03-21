10443bd30fc2e12f49a5cd193a19c9af25fea228
Evan <evan@bjjmapper.com>
2015-03-21 11:29:54 +0200

    Inelegant hack to fix directory listing and team select list

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -6,8 +6,8 @@ module TeamsHelper
     teams = TeamDecorator.decorate_collection(all_teams)
     grouped_teams = {}
     teams.each do |team|
-      grouped_teams[team.parent_team_id] ||= []
-      grouped_teams[team.parent_team_id] << team
+      grouped_teams[team.parent_team] ||= []
+      grouped_teams[team.parent_team] << team
     end
     grouped_teams
   end
