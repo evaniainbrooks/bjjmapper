bd4cc01b5dc3310b41542958dd92b0aacf26a9ae
Evan <evan@bjjmapper.com>
2015-05-26 14:55:17 +0300

    Confirm page navigation when in edit mode

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require confirm_page_exit
 //= require mixpanel
 //= require mixpanel_init
 //= require mixpanel_track_links
---
+++ app/assets/javascripts/confirm_page_exit.js
@@ -0,0 +1,25 @@
++function() {
+  function confirmPageExitHandler(e) {
+      // If we haven't been passed the event get the window.event
+      e = e || window.event;
+      var message = 'All of your changes will be lost.';
+
+      // For IE6-8 and Firefox prior to version 4
+      if (e) {
+          e.returnValue = message;
+      }
+
+      // For Chrome, Safari, IE8+ and Opera 12+
+      return message;
+  };
+
+  window.enableConfirmPageExit = function() {
+    window.onbeforeunload = confirmPageExitHandler; 
+  };
+
+  window.disableConfirmPageExit = function() {
+    window.onbeforeunload = null;
+  };
+
+}();
+
