73caf42383983135300a2a2d4f966ab5d086cf4c
Evan <evan@bjjmapper.com>
2017-01-08 14:44:13 +0300

    Remove force super user in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -98,11 +98,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    if Rails.env.development?
-      @current_user ||= User.where(:role => 'super_user').first
-      return @current_user
-    end
-
     # NORMAL USER AUTHENTICATION
     begin
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
