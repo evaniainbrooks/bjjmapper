757d494e76ed93f81a54960e2defc45544cf191d
Evan <evan@bjjmapper.com>
2017-01-27 23:38:41 +0300

    Fix spec

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -131,7 +131,7 @@ describe LocationsController do
       end
       context 'when there are locations nearby' do
         let(:other_location) { build(:location, title: 'near you location') }
-        before { Location.stub_chain(:near, :where, :verified, :limit, :to_a).and_return([location, other_location]) }
+        before { Location.stub_chain(:near, :where, :not_closed, :verified, :limit, :to_a).and_return([location, other_location]) }
         context 'with reject parameter' do
           it 'returns the nearby locations without the rejected location' do
             get :nearby, format: 'json', reject: location.to_param, lat: 80.0, lng: 80.0
@@ -154,7 +154,7 @@ describe LocationsController do
         end
       end
       context 'when there are no locations nearby' do
-        before { Location.stub_chain(:near, :where, :verified, :limit, :to_a).and_return([]) }
+        before { Location.stub_chain(:near, :where, :not_closed, :verified, :limit, :to_a).and_return([]) }
         it 'returns 204 no content' do
           get :nearby, format: 'json', lat: 80.0, lng: 80.0
           response.status.should eq 204
