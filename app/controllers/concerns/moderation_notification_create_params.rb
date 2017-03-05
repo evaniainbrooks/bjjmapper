446d8ed9849bb39c4964be60b4ab574d89e2dabf
Evan <evan@bjjmapper.com>
2017-03-06 00:21:19 +0300

    Allow all params for notifications

--- app/controllers/concerns/moderation_notification_create_params.rb
+++ app/controllers/concerns/moderation_notification_create_params.rb
@@ -2,8 +2,6 @@ module ModerationNotificationCreateParams
   extend ActiveSupport::Concern
   
   def moderation_notification_create_params
-    p = params.require(:notification).permit(:coordinates, :type, :info, :message, :source, :lat, :lng)
-    p[:coordinates] = [p[:lng], p[:lat]] if p[:coordinates].blank?
-    p
+    params.require(:notification).permit!
   end
 end
