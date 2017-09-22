9b575f17716db91283dc93503759cbd3ef1d90aa
Evan <evan@bjjmapper.com>
2017-09-22 21:12:32 +0200

    Check valid feature setting symbol

--- app/models/feature_setting.rb
+++ app/models/feature_setting.rb
@@ -8,11 +8,24 @@ class FeatureSetting
 
   validate :name, presence: true
 
+  VALID_FEATURE_SETTINGS = [
+    :hide_global_ads,
+    :hide_bjjatlas_events,
+    :hide_homepage_new_additions,
+    :hide_homepage_directory_segments,
+    :hide_locations_with_missing_street,
+    :show_activities 
+  ].freeze
+
   def self.enabled?(name)
+    raise ArgumentError, "#{name} is not a valid feature setting!" unless VALID_FEATURE_SETTINGS.include?(name)
+    
     FeatureSetting.where(name: name).first_or_initialize(value: false).value
   end
 
   def self.enable(name, value)
+    raise ArgumentError, "#{name} is not a valid feature setting!" unless VALID_FEATURE_SETTINGS.include?(name)
+
     FeatureSetting.where(name: name).first_or_initialize.tap do |feature|
       feature.value = value
       feature.save!
